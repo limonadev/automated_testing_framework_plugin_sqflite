fdfe3df57609de6bf2d25affea0e8f70626eaf09
limonadev <limonadev@whiteleaf.dev>
2020-09-16 14:12:15 -0500

    Export package files

--- lib/automated_testing_framework_plugin_sqflite.dart
+++ lib/automated_testing_framework_plugin_sqflite.dart
@@ -1,7 +1,2 @@
-library automated_testing_framework_plugin_sqflite;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:sqflite/sqflite.dart';
+export 'src/sqflite_test_store.dart';
--- test/automated_testing_framework_plugin_sqflite_test.dart
+++ test/automated_testing_framework_plugin_sqflite_test.dart
@@ -1,13 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:automated_testing_framework_plugin_sqflite/automated_testing_framework_plugin_sqflite.dart';
+//import 'package:automated_testing_framework_plugin_sqflite/automated_testing_framework_plugin_sqflite.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-  });
+  test('', () {});
 }
